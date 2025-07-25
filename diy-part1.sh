2f6ea08668d5a9ae5c3ab3d3289507c26576bed5
Fallpay <61930904+a905608594@users.noreply.github.com>
2025-07-26 00:42:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,17 +10,9 @@
 # See /LICENSE for more information.
 #
 
-# 取消注释 helloworld feed（如果存在被注释的）
+# Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# 添加 helloworld feed（如果不存在）
-grep -q "src-git helloworld" feeds.conf.default || echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-
-# 添加 passwall feed（如果不存在）
-grep -q "src-git passwall" feeds.conf.default || echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
-# 添加 smpackage feed（如果不存在）
-grep -q "src-git smpackage" feeds.conf.default || sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-
-# 可选：去除 feeds.conf.default 中重复行（防止多次执行导致重复）
-awk '!seen[$0]++' feeds.conf.default > feeds.conf.tmp && mv feeds.conf.tmp feeds.conf.default
+# Add a feed source
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
